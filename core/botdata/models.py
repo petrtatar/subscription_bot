71065037d3c4bdb1f665ddc732d8d66f27bf1303
petrtatar <tatar.petr523@gmail.com>
2025-11-05 00:53:58 +0300

    created UserProfile

--- core/botdata/models.py
+++ core/botdata/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    id = models.IntegerField(primary_key=True)
+    full_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.id}"
